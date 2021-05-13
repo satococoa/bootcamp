37a7b3800d6fa5d8bc62ac527ada3184cbdea602
teppei machida <machida@fjord.jp>
2021-05-14 00:03:15 +0900

    graduation_workのカラムが二重になっていたのを修正

--- db/schema.rb
+++ db/schema.rb
@@ -486,7 +486,6 @@ ActiveRecord::Schema.define(version: 2021_06_03_024907) do
     t.datetime "updated_at", null: false
     t.boolean "graduation_work", default: false, null: false
     t.index ["user_id", "title"], name: "index_works_on_user_id_and_title", unique: true
-    t.boolean "graduation_work", default: false, null: false
     t.index ["user_id"], name: "index_works_on_user_id"
   end
 
